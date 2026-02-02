56cd5c27cdbda85e19087cff99e4489b44605c18
Kosuke Mori <kosuke.mori.5120@gmail.com>
2026-02-02 13:02:20 +0900

    add crate

--- apps/auth/src/domain/repository/user_repository.rs
+++ apps/auth/src/domain/repository/user_repository.rs
@@ -1,7 +1,10 @@
 use crate::domain::error::app_error::AppError;
 use crate::domain::model::user::User;
+use uuid::Uuid;
 
+#[cfg_attr(test, mockall::automock)]
 pub trait UserRepository {
     async fn create_user(&self, user: &User) -> Result<(), AppError>;
     async fn find_user_by_email(&self, email: &str) -> Result<Option<User>, AppError>;
+    async fn find_user_by_id(&self, id: &Uuid) -> Result<Option<User>, AppError>;
 }
